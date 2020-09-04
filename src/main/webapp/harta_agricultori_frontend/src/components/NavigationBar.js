caa0e25aa6cea5a3dd0c90cb9f54ddd3b01c36cd
Gabriel Vlad <gabyvlad96@gmail.com>
2020-09-05 01:52:59 +0300

    login

--- src/main/webapp/harta_agricultori_frontend/src/components/NavigationBar.js
+++ src/main/webapp/harta_agricultori_frontend/src/components/NavigationBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Button} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 
 class NavigationBar extends React.Component {
@@ -12,8 +12,20 @@ render () {
                   <img src="./img/farm.png" width="25" height="25" alt="brand"/>
                   </Link>
                     <Nav className="mr-auto">
-                          <Link to={"parteneri"} className="nav-link">Parteneri</Link>
-                          <Link to={"addParteneri"} className="nav-link">Inscrie-te acum!</Link>
+                        <Link to={"parteneri"} className="nav-link">Parteneri</Link>
+                        <Link to={"addParteneri"} className="nav-link">Inscrie-te acum!</Link>
+                        {this.props.isAuthed &&
+                            <Link to={"adminPage"} className="nav-link">Admin Page</Link>
+                        }
+                        {!this.props.isAuthed &&
+                            <Link to={"login"} className="nav-link">Login</Link>
+                        }
+                        {!this.props.isAuthed &&
+                            <Link to={"register"} className="nav-link">Register</Link>
+                        }
+                        {this.props.isAuthed &&
+                            <Button onClick={this.props.logout} className="nav-link">Logout</Button>
+                        }
                     </Nav>
                   </Navbar>
             </div>
---
+++ src/main/webapp/harta_agricultori_frontend/src/components/auth/Register.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import "./auth.css";
+
+const axios = require('axios').default;
+
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      email: "",
+      password1: "",
+      password2: ""
+    };
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = () => {
+    const { username, email, password1, password2 } = this.state;
+    console.log("Submitting");
+    axios.post('http://127.0.0.1:8000/rest-auth/registration/', {
+      username,
+      email,
+      password1,
+      password2
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  };
+
+  render() {
+    const { username, email, password1, password2 } = this.state;
+    return (
+      <div>
+        <h2 id="text">Register</h2>
+        <form>
+          <label htmlFor="email">Username</label>
+          <input
+            name="username"
+            type="text"
+            placeholder="Enter your username"
+            value={username}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="email">Password</label>
+          <input
+            name="password1"
+            type="password"
+            value={password1}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="email">Re-enter your password</label>
+          <input
+            name="password2"
+            type="password"
+            value={password2}
+            onChange={this.handleChange}
+          />
+          <button className="authButton" type="button" onClick={this.handleSubmit}>Register</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Register;
